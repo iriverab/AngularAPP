bc68a17d01bff2d7299f31fb4d05aef8ec80f94d
iriverab <iriverab82@gmail.com>
2019-11-26 16:15:24 -0300

    Final commit
    Cambios Finales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 /*Aca definimos el ruteo de componentes que generamos(solo principales)*/
-const routes: Routes = [{ path: 'Home', component: HomeComponent }];
+const routes: Routes = [{ path: 'Home', component: HomeComponent },
+{ path: '', component: HomeComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
